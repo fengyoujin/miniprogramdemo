38075d1dc50a05798205a51aa96bffc45f2b04f7
Eugene Feng <Eugene_Feng@outlook.com>
2022-03-05 12:28:36 +0800

    P01~P20

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# miniprogramdemo
-fengyoujin/miniprogramdemo
+# 目录
+## [数据绑定](/miniprogram/pages/demo01/demo01.wxml)
+## [运算](/miniprogram/pages/demo01/demo01.wxml)
+## [列表循环](/miniprogram/pages/demo01/demo01.wxml)
+## [对象循环](/miniprogram/pages/demo01/demo01.wxml)
+## [block占位标签](/miniprogram/pages/demo01/demo01.wxml)
+## [条件渲染](/miniprogram/pages/demo01/demo01.wxml)
+
+## [事件绑定](/miniprogram/pages/demo01/demo02.wxml)
+
+## [WXSS](/miniprogram/pages/demo01/demo02.wxml)
+### 
---
+++ miniprogram/pages/demo01/demo01.ts
@@ -0,0 +1,93 @@
+// pages/demo01/demo01.ts
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        msg:'hello world',
+        num:100,
+        isGirl:true,
+        person:{
+            age:20,
+            height:178,
+            weight:180,
+            name:'小明'
+        },
+        isChecked:true,
+        list:[
+            {
+                id:0,
+                name:'孙悟空'
+            },
+            {
+                id:1,
+                name:'唐僧'
+            },
+            {
+                id:2,
+                name:'猪八戒'
+            },
+            {
+                id:3,
+                name:'沙和尚'
+            },
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
